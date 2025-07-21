11de271642105c70840c5f7cd3f91417e8c19523
suvitcha <boom.suvitcha@gmail.com>
2025-07-21 11:53:19 +0700

    Morning Checkpoint

--- main.js
+++ main.js
@@ -17,20 +17,39 @@ class Field {
 
 		// Replace with your own code //
 		// Set the home position at (0, 0) before the game starts
-		this.positionRow = 0;
-		this.positionCol = 0;
+		this.positionRow = 0; //up-down
+		this.positionCol = 0; //l-r
 		this.field[this.positionRow][this.positionCol] = pathCharacter;
 	}
 
 	// Print field //
 	print() {
-		clear();
+		//clear();
 
 		// Replace with your own code //
-		console.log(this.field); // Please REMOVE this line before you start your code!
+		//console.log(this.field); // Please REMOVE this line before you start your code!
+		const printMatrix = this.field.map((d) => d.join(" ")).join("\n");
+		console.log(printMatrix);
 	}
 
 	// Your Code //
+
+	game() {
+		while (true) {
+		let n = prompt("Which Way ? :");
+			if (n === 'r') {
+				this.positionCol++;
+			} else if(n === 'd'){
+				this.positionRow++;
+			} else if(n === 'l'){
+				this.positionCol--;
+			} else if(n === 'u'){
+				this.positionRow--;
+			}
+		this.field[this.positionRow][this.positionCol] = pathCharacter;
+		this.print();
+		}
+	}
 }
 
 // Game Mode ON
@@ -41,3 +60,4 @@ const newGame = new Field([
 	["░", "^", "░"],
 ]);
 newGame.print();
+newGame.game();
